6886ba28f2c6ff910bc293839b92fa1f409d2a55
Rossan P <53863541+rossanp@users.noreply.github.com>
2020-04-15 13:10:47 -0300

    Criação
    Criação do exercício.

---
+++ src/meuslivros/MeusLivros.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meuslivros;
+
+import Views.InitView;
+
+/**
+ *
+ * @author Rossan
+ */
+public class MeusLivros {
+    
+    public static void main(String[] args) {
+    InitView telaInicial = new InitView();
+    
+    telaInicial.setVisible(true);
+    
+    }
+}
